987bc8def62ee19d3394fb5bcec34fcb5fd381dd
Adbrap <adprog33@gmail.com>
2023-01-17 12:12:28 +0100

    first commit

---
+++ launcher.sh
@@ -0,0 +1,138 @@
+clear
+
+echo "                                                                         "
+echo "                                                                         "
+echo "                                                                         "
+echo "                                     ▄▄▄      ▓█████▄  ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓"
+echo "                                    ▒████▄    ▒██▀ ██▌▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒"
+echo "                                    ▒██  ▀█▄  ░██   █▌▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░"
+echo "                                    ░██▄▄▄▄██ ░▓█▄   ▌▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ "
+echo "                                     ▓█   ▓██▒░▒████▓ ▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ "
+echo "                                     ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░ "
+echo "                                     ▒   ▒▒ ░ ░ ▒  ▒ ░  ░      ░  ▒   ▒▒ ░   ░  "
+echo "                                    ░   ▒    ░ ░  ░ ░      ░     ░   ▒    ░   "
+echo "                                        ░  ░   ░           ░         ░  ░   "
+echo "                                             ░     "
+
+
+
+while :
+do
+
+  python3 /home/ubuntu/Desktop/admatv3.7/BTC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ETH.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/XRP.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ADA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SOL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/DOGE.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/MATIC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/DOT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/DAI.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SHIB.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/TRX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/AVAX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/UNI.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/WBTC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/LTC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ATOM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/LINK.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ETC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/XLM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/FTT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/NEAR.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/CRO.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/XMR.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ALGO.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BCH.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/QNT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/FLOW.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/VET.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/APE.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/FIL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ICP.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/EGLD.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/XTZ.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/MANA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SAND.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/CHZ.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/EOS.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/AAVE.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/THETA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/AXS.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BSV.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/MKR.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ZEC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BTT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/GRT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/NEO.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/FTM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SNX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/NEXO.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/DASH.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/CRV.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BAT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ENJ.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/STX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/COMP.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ZIL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/WAVES.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/XDC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/LRC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/KSM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BTG.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/1INCH.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/LUNA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/GUSD.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ROSE.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/YFI.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/QTUM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ANKR.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/GNO.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/GALA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/IOTX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/GLM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BTRST.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/JST.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/CEL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/POLY.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/OMG.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/LPT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ZRX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/BAL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ICX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/AMP.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SRM.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/KNC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/CHSB.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/STORJ.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/ZEN.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SUSHI.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/IMX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/UMA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/PLA.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SKL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/DGB.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/VGX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/CVC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/LSK.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/COTI.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/KEEP.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/REN.py &
+  sleep 6
+  python3 /home/ubuntu/Desktop/admatv3.7/REN.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/RNDR.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/REEF.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/NU.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/REQ.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SPELL.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/RLC.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/OCEAN.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/API3.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/SNT.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/TRIBE.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/FX.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/UOS.py &
+  python3 /home/ubuntu/Desktop/admatv3.7/%launcher%
+
+done
